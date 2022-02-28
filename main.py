dce5593982ba404407dea0b7ca89a7a6c78de4f2
Josh Tan <prof.whale@gmail.com>
2022-02-28 17:43:20 +0100

    validating data with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from fastapi.param_functions import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/") #decorator, tells what path url
 def root():
@@ -12,7 +22,9 @@ def root():
 def get_posts():
     return {"data": "this is posts"}
 
+
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
