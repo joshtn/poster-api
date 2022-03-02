1649920a1a5c217ce2971b996f42103feb4e3706
Josh Tan <prof.whale@gmail.com>
2022-03-02 15:36:15 +0100

    CRUD delete implemented with status code handling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.param_functions import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+
+from starlette.status import HTTP_204_NO_CONTENT
 
 app = FastAPI()
 
@@ -21,6 +23,13 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+
+def find_index_post(id):
+    for i, p  in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return {"message": "Goat first api!!"}
@@ -31,7 +40,7 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 100000)
@@ -41,4 +50,17 @@ def create_posts(post: Post):
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id: {id} was not found")
+#        response.status_code = status.HTTP_404_NOT_FOUND
+#        return {"message": f"Post with id: {id} was not found"}
     return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=HTTP_204_NO_CONTENT)
