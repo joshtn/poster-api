78f6cb3aa2016201fb6799a4bdf0b524f5c83497
Josh Tan <prof.whale@gmail.com>
2022-04-27 15:10:26 +0200

    cleanup main file and move old stuff to database.py

--- app/database.py
+++ app/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .keys import SQLALC_DB_URL
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 SQLALCHEMY_DATABASE_URL = SQLALC_DB_URL
 
@@ -19,3 +22,34 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# not using this raw sql. Now using sqlalchemy
+# while True:
+
+#     try:
+#         conn = psycopg2.connect(host=keys.HOST, database=keys.DB, user=keys.USER, password=keys.PASSW, cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successfull!")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
+
+
+# used this before 
+# my_posts = [{"title": "post 1", "content": "content 1", "id": 1},{"title": "best pizza", "content": "kebab", "id": 2}]
+
+# def find_post(id):
+#     for p in my_posts:
+#         if p["id"] == id:
+#             return p
+
+
+# def find_index_post(id):
+#     for i, p  in enumerate(my_posts):
+#         if p["id"] == id:
+#             return i
+
+
--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI, Depends
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from sqlalchemy.orm import Session
-from . import models, keys
+from . import models
 from .database import engine, get_db
 from .routers import post, user, auth
 
@@ -11,34 +8,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-while True:
-
-    try:
-        conn = psycopg2.connect(host=keys.HOST, database=keys.DB, user=keys.USER, password=keys.PASSW, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
-
-
-my_posts = [{"title": "post 1", "content": "content 1", "id": 1},{"title": "best pizza", "content": "kebab", "id": 2}]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p  in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-
 
 app.include_router(post.router)
 app.include_router(user.router)
