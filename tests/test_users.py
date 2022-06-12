4c18e721baf75eda9d92a583af9b2ce7fbf017f3
Josh Tan <prof.whale@gmail.com>
2022-06-12 19:20:20 +0200

    test: adding fixtures, seperating test database code

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,44 +1,14 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.main import app
 from app import schemas
-from app.config import settings
-from app.database import get_db
-from app.database import Base
+from .database import client, session
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get("How to use api"))
     assert res.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json={"email": "pytest1@gmail.com", "password": "test123"})
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "pytest1@gmail.com"
