55fd5c9ba128cb5f25115eac696ece8a2f90a1c9
Josh Tan <prof.whale@gmail.com>
2022-06-13 13:00:58 +0200

    test: writing more tests for users, moving shared fixtures to conftest.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,13 @@
 from app import schemas
-from .database import client, session
+from jose import jwt
+import pytest
+from app.config import settings
 
 
-def test_root(client):
-    res = client.get("/")
-    print(res.json().get("How to use api"))
-    assert res.status_code == 200
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get("How to use api"))
+#     assert res.status_code == 200
 
 
 def test_create_user(client):
@@ -14,3 +16,24 @@ def test_create_user(client):
     assert new_user.email == "pytest1@gmail.com"
     assert res.status_code == 201
 
+
+def test_login_user(client, test_user):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'test123', 403),
+    ('pytest1@gmail.com', 'est123', 403),
+    ('wrongemail@gmail.com', 'ghghghf', 403),
+    (None,'wrongPass' 'asdasdf', 403),
+    ('pytest1@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
