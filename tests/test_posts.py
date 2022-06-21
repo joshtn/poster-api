4b721b44836a2bfb408e8e2ea31c7192d6804315
Josh Tan <prof.whale@gmail.com>
2022-06-21 10:53:13 +0200

    test: all tests for post written

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -99,3 +99,29 @@ def test_updated_post(authorized_client, test_user, test_posts):
     assert updated_post.title == data['title']
     assert updated_post.content == data['content']
 
+
+def test_update_other_user_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
+
+
+def test_unauthorized_user_update_post(client, test_user, test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put("/posts/123456789", json=data)
+    assert res.status_code == 404
+
